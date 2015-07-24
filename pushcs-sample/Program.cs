dafd08a97b6770f3c7d5c1de27bb8fcb2bd3306c
Kaelan Fouwels <kfouwels@outlook.com>
2015-07-24 18:25:12 +0100

    added async shim client

--- pushcs-sample/Program.cs
+++ pushcs-sample/Program.cs
@@ -8,23 +8,28 @@ namespace pushcs_sample
 {
     public class Program
     {
-        public void Main(string[] args)
+        public async void Main(string[] args)
         {
 			var testkey = "this_is_a_60_char_string_that_looks_like_a_valid_private_key";
 
-			//call api endpoints directly. Check the method tooltips, or XML comments directly
-			var ap = new pushcs.Api.ApiRoot(
-				BaseUrl: "http://live.pushman.dfl.mn/", UserAgent: "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+			//calling api endpoints directly. Check the method tooltips, or XML comments directly
+			var api = new pushcs.Api.ApiRoot(
+				BaseUrl: "http://live.pushman.dfl.mn/", 
+				UserAgent: "Mozilla/5.0 (Windows NT 6.1) appleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
 
-			var a =	ap.Push(Private: testkey, Event: "ayy", Payload: "{\"ayy\" : \"lemayo\"}");
-			var b =	ap.Channels(Private: testkey);
-			var c =	ap.ChannelGet(Private: testkey, Channel: "auth");
-			var d =	ap.ChannelPost(Private: testkey, Channel: "auth");
-			var e = ap.ChannelDelete(Private: testkey, Channel: "auth");
-			var f =	ap.Subscribers(Private: testkey, Channel: "auth");
+			var a =	api.Push(Private: testkey, Event: "ayy", Payload: "{\"ayy\" : \"lemayo\"}");
+			var b =	api.Channels(Private: testkey);
+			var c =	api.ChannelGet(Private: testkey, Channel: "auth");
+			var d =	api.ChannelPost(Private: testkey, Channel: "auth");
+			var e = api.ChannelDelete(Private: testkey, Channel: "auth");
+			var f =	api.Subscribers(Private: testkey, Channel: "auth");
 
-			var client = new pushcs.Client.Client();
+			//using the shim client
+			var client = new pushcs.Client(SitePrivateKey: testkey, HostUrl: "http://live.pushman.dfl.mn/");
 
-        }
+			await client.PushEvent(Name: "ayy", Data: "{ \"ayy\" : \"lmao\" }");
+			//etc.
+
+		}
     }
 }
---
+++ pushcs/Client/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using pushcs.Api;
+
+namespace pushcs
+{
+    public class Client
+    {
+		private ApiRoot ap;
+		private string _key;
+		public Client(string SitePrivateKey, string HostUrl = "http://live.pushman.dfl.mn", string UserAgent = "")
+        {
+			_key = SitePrivateKey;
+			var userAgent = UserAgent;
+			var baseUrl = (HostUrl.ToArray().Last() != '/') ? HostUrl : HostUrl.Remove(HostUrl.Length - 1);
+			ap = new ApiRoot(baseUrl, userAgent);
+
+		}
+
+		public async Task<Api.Responses.PushResponse> PushEvent(string Name, List<string> Channels = null, string Data)
+		{
+			return ap.Push(_key, Name, string.Join(",", Channels), Data);
+		}
+		public async Task<Api.Responses.ChannelsResponse> GetMyChannels()
+		{
+			return ap.Channels(_key);
+		}
+		public async Task<Api.Responses.ChannelResponseGet> GetChannelDetails(string Channel)
+		{
+			return ap.ChannelGet(_key, Channel);
+		}
+		public async Task<Api.Responses.ChannelResponsePost> CreateChannel(string Channel, int MaxConnections = 0, bool refreshTokenEvery60 = false)
+		{
+			return ap.ChannelPost(_key, Channel, MaxConnections, refreshTokenEvery60); 
+		}
+		public async Task<Api.Responses.ChannelResponseDelete> DropChannel(string Channel)
+		{
+			return ap.ChannelDelete(_key, Channel);
+		}
+		public async Task<Api.Responses.SubscribersResponse> GetSubscribers(string Channel)
+		{
+			return ap.Subscribers(_key, Channel);
+		}
+    }
+}
