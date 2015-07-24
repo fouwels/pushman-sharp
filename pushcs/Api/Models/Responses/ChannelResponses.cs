c054fddbc0f63d2fd3683fe79caf54fda3e75c5c
Kaelan Fouwels <kfouwels@outlook.com>
2015-07-24 14:46:02 +0100

    re-shuffle

--- pushcs/Api/Models/Responses/ChannelResponses.cs
+++ pushcs/Api/Models/Responses/ChannelResponses.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace pushcs.Models
+namespace pushcs.Api.Models.Responses
 {
 	public class ChannelResponseGet
 	{
